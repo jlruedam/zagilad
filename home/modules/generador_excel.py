310733d5e1b6d512757fbf4fcafaa0e59d3f8ae5
jlruedam <jlruedam@gmail.com>
2024-09-18 15:03:14 -0500

    Depurar procesamiento de exporte a excel de la carga

--- home/modules/generador_excel.py
+++ home/modules/generador_excel.py
@@ -4,64 +4,27 @@ import datetime
 
 #-------------------------------------------------------
 def genera_excel_carga(actividades_carga):
+
     dict_carga = {
-        'id':[],
-        'tipo_fuente':[],
-        'admision':[],
-        'identificador':[],
-        'regional':[],
-        'fecha_servicio':[],
-        'nombre_actividad':[],
-        # 'tipo_actividad':[],
-        'diagnostico_p':[],
-        'diagnostico_1':[],
-        'diagnostico_2':[],
-        'diagnostico_3':[],
-        'tipo_documento':[],
-        'documento_paciente':[],
-        'nombre_paciente':[],
-        'parametros_programa':[],
-        'carga':[],
-        'inconsistencias':[],
-        'created_at':[],
-        'updated_at':[],
+        'id':list(actividades_carga.values_list('id', flat=True)),
+        'admision':list(actividades_carga.values_list('admision', flat=True)),
+        'identificador':list(actividades_carga.values_list('identificador', flat=True)),
+        'regional':list(actividades_carga.values_list('regional', flat=True)),
+        'fecha_servicio':list(actividades_carga.values_list('fecha_servicio', flat=True)),
+        'nombre_actividad':list(actividades_carga.values_list('nombre_actividad', flat=True)),
+        'diagnostico_p':list(actividades_carga.values_list('diagnostico_p', flat=True)),
+        'diagnostico_1':list(actividades_carga.values_list('diagnostico_1', flat=True)),
+        'diagnostico_2':list(actividades_carga.values_list('diagnostico_2', flat=True)),
+        'diagnostico_3':list(actividades_carga.values_list('diagnostico_3', flat=True)),
+        'tipo_documento':list(actividades_carga.values_list('tipo_documento', flat=True)),
+        'documento_paciente':list(actividades_carga.values_list('documento_paciente', flat=True)),
+        'nombre_paciente':list(actividades_carga.values_list('nombre_paciente', flat=True)),
+        'carga':list(actividades_carga.values_list('carga', flat=True)),
+        'inconsistencias':list(actividades_carga.values_list('inconsistencias', flat=True)),
     }
 
-    print(dict_carga.keys())
-
-    
-    for carga in actividades_carga:
-        
-        dict_carga["id"].append(carga.id)
-        dict_carga["tipo_fuente"].append(str(carga.tipo_fuente))
-        dict_carga["admision"].append(carga.admision.numero_estudio if carga.admision else "")
-        dict_carga["identificador"].append(carga.identificador)
-        dict_carga["regional"].append(carga.regional)
-        dict_carga["fecha_servicio"].append(str(carga.fecha_servicio))
-        dict_carga["nombre_actividad"].append(carga.nombre_actividad)
-        # dict_carga["tipo_actividad"].append(carga.tipo_actividad)
-        dict_carga["diagnostico_p"].append(carga.diagnostico_p)
-        dict_carga["diagnostico_1"].append(carga.diagnostico_1)
-        dict_carga["diagnostico_2"].append(carga.diagnostico_2)
-        dict_carga["diagnostico_3"].append(carga.diagnostico_3)
-        dict_carga["tipo_documento"].append(carga.tipo_documento)
-        dict_carga["documento_paciente"].append(carga.documento_paciente)
-        dict_carga["nombre_paciente"].append(carga.nombre_paciente)
-        dict_carga["parametros_programa"].append(carga.parametros_programa)
-        dict_carga["carga"].append(carga.carga)
-        dict_carga["inconsistencias"].append(carga.inconsistencias)
-        dict_carga["created_at"].append(str(carga.created_at))
-        dict_carga["updated_at"].append(str(carga.updated_at))
-
-    print(dict_carga)
-
     df = pd.DataFrame(dict_carga)
-    # df = df[[
-    #     "id","fecha_solicitud","solicitud_asociada","estado","colaborador",
-    #     "tipo_operacion","observaciones", "valor", "created_at", "updated_at"
-    # ]]
     df = df[dict_carga.keys()]
-
 
     writer = ExcelWriter('media//listado_actividades_carga.xlsx')
     df.to_excel(writer, 'Hoja de datos', index = False)
