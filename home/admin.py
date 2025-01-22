65f500632b02698cc566275bc1f190806ff5dec1
jlruedam <jlruedam@gmail.com>
2025-01-22 09:52:33 -0500

    Agregar configuraciones de administración para varios modelos, incluyendo agrupación de campos, campos de solo lectura, y opciones de listado y búsqueda.

--- home/admin.py
+++ home/admin.py
@@ -5,32 +5,308 @@ from home import models
 
 
 class AdmisionAdmin(admin.ModelAdmin):
-    pass
+    # Agrupación de campos con fieldsets
+    fieldsets = (
+        ('Información del Paciente', {
+            'fields': ('documento_paciente', 'numero_estudio')
+        }),
+        ('Datos JSON', {
+            'fields': ('json',),
+            'classes': ('collapse',),  # Sección plegable
+        }),
+        ('Tiempos', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',),  # Sección plegable
+        }),
+    )
+
+    # Campos de solo lectura
+    readonly_fields = ('created_at', 'updated_at')
+
+    # Listado
+    list_display = ('id', 'documento_paciente', 'numero_estudio', 'created_at', 'updated_at')
+
+    # Filtros laterales
+    list_filter = ('created_at', 'updated_at')
+
+    # Campos de búsqueda
+    search_fields = ('documento_paciente', 'numero_estudio')
+
+    # Orden predeterminado
+    ordering = ('-created_at',)
 
 class AreaProgramaAdmin(admin.ModelAdmin):
-    pass
+    # Agrupación de campos con fieldsets
+    fieldsets = (
+        ('Información General', {
+            'fields': ('identificador', 'nombre')
+        }),
+        ('Tiempos', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',),  # Sección plegable
+        }),
+    )
+
+    # Campos de solo lectura
+    readonly_fields = ('created_at', 'updated_at')
+
+    # Listado
+    list_display = ('id', 'identificador', 'nombre', 'created_at', 'updated_at')
+    
+    # Filtros laterales
+    list_filter = ('created_at', 'updated_at')
+
+    # Campos de búsqueda
+    search_fields = ('identificador', 'nombre')
+
+    # Orden predeterminado
+    ordering = ('-created_at',)
 
 class RegionalAdmin(admin.ModelAdmin):
-    pass
+    # Agrupación de campos con fieldsets
+    fieldsets = (
+        ('Información General', {
+            'fields': ('regional',)
+        }),
+        ('Tiempos', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',),  # Sección plegable
+        }),
+    )
+
+    # Campos de solo lectura
+    readonly_fields = ('created_at', 'updated_at')
+
+    # Listado
+    list_display = ('id', 'regional', 'created_at', 'updated_at')
+
+    # Filtros laterales
+    list_filter = ('created_at', 'updated_at')
+
+    # Campos de búsqueda
+    search_fields = ('regional',)
+
+    # Orden predeterminado
+    ordering = ('-created_at',)
 
 class ContratoMarcoAdmin(admin.ModelAdmin):
-    pass
+    # Agrupación de campos con fieldsets
+    fieldsets = (
+        ('Información del Contrato', {
+            'fields': ('numero', 'contrato_subsidiado', 'contrato_contributivo')
+        }),
+        ('Detalles Adicionales', {
+            'fields': ('observacion',),
+        }),
+        ('Tiempos', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',),  # Sección plegable
+        }),
+    )
+
+    # Campos de solo lectura
+    readonly_fields = ('created_at', 'updated_at')
+
+    # Listado
+    list_display = ('id', 'numero', 'contrato_subsidiado', 'contrato_contributivo', 'created_at', 'updated_at')
+
+    # Filtros laterales
+    list_filter = ('created_at', 'updated_at')
+
+    # Campos de búsqueda
+    search_fields = ('numero', 'observacion')
+
+    # Orden predeterminado
+    ordering = ('-created_at',)
 
 class ParametrosAreaProgramaAdmin(admin.ModelAdmin):
-    pass
+    # Agrupación de campos con fieldsets
+    fieldsets = (
+        ('Relaciones Principales', {
+            'fields': ('area_programa', 'regional', 'unidad_funcional', 'punto_atencion', 'centro_costo', 'sede')
+        }),
+        ('Tiempos', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',),  # Sección plegable
+        }),
+    )
+
+    # Campos de solo lectura
+    readonly_fields = ('created_at', 'updated_at')
+
+    # Listado
+    list_display = (
+        'id', 'area_programa', 'regional', 'unidad_funcional', 
+        'punto_atencion', 'centro_costo', 'sede', 'created_at', 'updated_at'
+    )
+
+    # Filtros laterales
+    list_filter = ('area_programa', 'regional', 'sede', 'created_at', 'updated_at')
+
+    # Campos de búsqueda
+    search_fields = ('area_programa__nombre', 'regional__regional', 'unidad_funcional__nombre', 'centro_costo__nombre')
+
+    # Orden predeterminado
+    ordering = ('-created_at',)
 
 class TipoActividadAdmin(admin.ModelAdmin):
-    pass
+    # Agrupación de campos con fieldsets
+    fieldsets = (
+        ('Información General', {
+            'fields': ('grupo', 'nombre', 'cups', 'responsable', 'diagnostico')
+        }),
+        ('Detalles Adicionales', {
+            'fields': ('finalidad', 'fuente', 'observacion', 'entrega'),
+            'classes': ('collapse',),  # Sección plegable
+        }),
+        ('Relaciones', {
+            'fields': ('contrato', 'tipo_servicio', 'area'),
+            'classes': ('collapse',),  # Sección plegable
+        }),
+        ('Tiempos', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',),  # Sección plegable
+        }),
+    )
+
+    # Campos de solo lectura
+    readonly_fields = ('created_at', 'updated_at')
+
+    # Listado
+    list_display = ('id', 'nombre', 'cups', 'grupo', 'contrato', 'tipo_servicio', 'area', 'created_at')
+    
+    # Filtros laterales
+    list_filter = ('grupo', 'tipo_servicio', 'area', 'created_at')
+
+    # Campos de búsqueda
+    search_fields = ('nombre', 'cups', 'responsable', 'diagnostico', 'finalidad', 'fuente', 'observacion')
+
+    # Orden predeterminado
+    ordering = ('-created_at',)
 
 class ActividadAdmin(admin.ModelAdmin):
-    pass
+    # Agrupación de campos con fieldsets
+    fieldsets = (
+        ('Información General', {
+            'fields': (
+                'tipo_fuente', 'identificador', 'fecha_servicio',
+                'nombre_actividad', 'tipo_actividad', 'regional',
+            )
+        }),
+        ('Datos del Paciente', {
+            'fields': (
+                'tipo_documento', 'documento_paciente', 'nombre_paciente',
+                'diagnostico_p', 'diagnostico_1', 'diagnostico_2', 'diagnostico_3',
+            )
+        }),
+        ('Relaciones y Estado', {
+            'fields': (
+                'admision', 'parametros_programa', 'carga', 
+                'inconsistencias', 'admisionada_otra_carga', 'datos_json',
+            )
+        }),
+        ('Información del Médico', {
+            'fields': ('documento_medico', 'medico'),
+        }),
+        ('Tiempos', {
+            'fields': ('created_at', 'updated_at'),
+            'classes': ('collapse',),  # Sección plegable
+        }),
+    )
 
-# class ColaboradorAdmin(admin.ModelAdmin):
-#     pass
+    # Campos de solo lectura
+    readonly_fields = ('created_at', 'updated_at')
+
+    # Listado
+    list_display = (
+        'id', 'tipo_fuente', 'fecha_servicio', 'nombre_actividad', 
+        'documento_paciente', 'nombre_paciente', 'carga', 
+        'admision', 'created_at', 'updated_at',
+    )
+
+    # Filtros laterales
+    list_filter = (
+        'tipo_fuente', 'fecha_servicio', 'tipo_actividad', 
+        'regional', 'carga', 'admisionada_otra_carga', 'created_at'
+    )
+
+    # Campos de búsqueda
+    search_fields = (
+        'tipo_fuente', 'identificador', 'nombre_actividad', 
+        'documento_paciente', 'nombre_paciente', 'regional',
+    )
+
+    # Orden predeterminado
+    ordering = ('-created_at',)
 
 class CargaAdmin(admin.ModelAdmin):
-    pass
+    # Agrupar campos con fieldsets
+    fieldsets = (
+        ('Información General', {
+            'fields': ('usuario', 'estado', 'observacion')
+        }),
+        ('Estadísticas de Actividades', {
+            'fields': (
+                'cantidad_actividades',
+                'cantidad_actividades_ok',
+                'cantidad_actividades_inconsistencias',
+                'cantidad_actividades_admisionadas',
+            ),
+            'classes': ('collapse',),  # Hace que esta sección sea colapsable
+        }),
+        ('Detalles de Tiempo', {
+            'fields': ('tiempo_procesamiento', 'created_at', 'updated_at'),
+            'classes': ('collapse',),  # También colapsable
+        }),
+    )
 
+    # Campos de solo lectura
+    readonly_fields = ('created_at', 'updated_at', 'cantidad_actividades', 'cantidad_actividades_ok', 
+                        'cantidad_actividades_inconsistencias', 'cantidad_actividades_admisionadas')
+
+    # Opciones adicionales (opcional)
+    list_display = ('id', 'usuario', 'estado', 'cantidad_actividades', 'created_at')
+    search_fields = ('observacion', 'usuario__username')
+    list_filter = ('estado', 'created_at')
+    # Campos que se mostrarán en el listado del modelo
+    list_display = (
+        'id',
+        'usuario',
+        'estado',
+        'cantidad_actividades',
+        'cantidad_actividades_ok',
+        'cantidad_actividades_inconsistencias',
+        'cantidad_actividades_admisionadas',
+        'observacion',
+        'created_at',
+        'updated_at',
+    )
+
+    # Campos para búsqueda
+    search_fields = ('observacion', 'usuario__username')
+
+    # Filtros laterales
+    list_filter = ('estado', 'created_at', 'updated_at')
+
+    # Campos de solo lectura (no editables desde el admin)
+    readonly_fields = (
+        'id',
+        'created_at',
+        'updated_at',
+        'cantidad_actividades',
+        'cantidad_actividades_ok',
+        'cantidad_actividades_inconsistencias',
+        'cantidad_actividades_admisionadas',
+    )
+
+    # Orden predeterminado del listado
+    ordering = ('-created_at',)  # Orden descendente por fecha de creación
+
+class TokenApiZeusAdmin(admin.ModelAdmin):
+    list_display = ('id', 'token', 'vigente', 'vigencia', 'updated_at')
+    list_filter = ('vigente', 'updated_at')
+    search_fields = ('token',)
+    readonly_fields = ('created_at', 'updated_at')
 
 admin.site.register(models.Admision, AdmisionAdmin)
 admin.site.register(models.AreaPrograma, AreaProgramaAdmin)
@@ -39,6 +315,6 @@ admin.site.register(models.ContratoMarco, ContratoMarcoAdmin)
 admin.site.register(models.ParametrosAreaPrograma, ParametrosAreaProgramaAdmin)
 admin.site.register(models.TipoActividad, TipoActividadAdmin)
 admin.site.register(models.Actividad, ActividadAdmin)
-# admin.site.register(models.Colaborador, ColaboradorAdmin)
 admin.site.register(models.Carga, CargaAdmin)
+admin.site.register(models.TokenApiZeus, TokenApiZeusAdmin)
 
--- home/models.py
+++ home/models.py
@@ -4,166 +4,202 @@ from zeus_mirror.models import Contrato, UnidadFuncional, CentroCosto, PuntoAten
 from zeus_mirror.models import Sede, TipoServicio, Medico
 import datetime
 from datetime import timezone
+
 # Create your models here.
 
+
 class Admision(models.Model):
-    id = models.AutoField(primary_key =True)
+    id = models.AutoField(primary_key=True)
     documento_paciente = models.IntegerField()
     numero_estudio = models.IntegerField()
-    json = models.JSONField(blank=True,null=True)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    json = models.JSONField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Admision - {self.id} - {self.documento_paciente} - {self.numero_estudio}'
+        return f"Admision_{self.id}_{self.documento_paciente}_{self.numero_estudio}"
+
 
 class AreaPrograma(models.Model):
-    id = models.AutoField(primary_key =True) 
+    id = models.AutoField(primary_key=True)
     identificador = models.CharField(max_length=10, unique=True)
     nombre = models.CharField(max_length=150)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
-        return f'AreaPrograma - {self.id} - {self.identificador} - {self.nombre}' 
+        return f"AreaPrograma_{self.id}_{self.identificador}_{self.nombre}"
+
 
 class Regional(models.Model):
-    id = models.AutoField(primary_key =True) 
+    id = models.AutoField(primary_key=True)
     regional = models.CharField(max_length=150)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
-        return f'Regional - {self.id} - {self.regional}' 
+        return f"Regional_{self.id}_{self.regional}"
+
 
 class ContratoMarco(models.Model):
-    id = models.AutoField(primary_key =True)
-    numero = models.CharField(max_length = 50, unique=True)
-    contrato_subsidiado = models.ForeignKey(Contrato, models.SET_NULL, blank=True,null=True, related_name='subsidado') 
-    contrato_contributivo = models.ForeignKey(Contrato, models.SET_NULL, blank=True,null=True, related_name='contributivo') 
-    observacion = models.CharField(max_length = 150, blank=True,null=True)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    id = models.AutoField(primary_key=True)
+    numero = models.CharField(max_length=50, unique=True)
+    contrato_subsidiado = models.ForeignKey(
+        Contrato, models.SET_NULL, blank=True, null=True, related_name="subsidado"
+    )
+    contrato_contributivo = models.ForeignKey(
+        Contrato, models.SET_NULL, blank=True, null=True, related_name="contributivo"
+    )
+    observacion = models.CharField(max_length=150, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'ContratoMarco- {self.id} - {self.numero} - {self.contrato_subsidiado} - {self.contrato_contributivo}'
-       
+        return f"ContratoMarco_{self.id}_{self.numero}_{self.contrato_subsidiado}_{self.contrato_contributivo}"
+
+
 class ParametrosAreaPrograma(models.Model):
-    id = models.AutoField(primary_key =True)
-    area_programa = models.ForeignKey(AreaPrograma, models.SET_NULL, blank=True,null=True) 
-    regional = models.ForeignKey(Regional, models.SET_NULL, blank=True,null=True) 
-    unidad_funcional = models.ForeignKey(UnidadFuncional, models.SET_NULL, blank=True,null=True) 
-    punto_atencion = models.ForeignKey(PuntoAtencion, models.SET_NULL, blank=True,null=True) 
-    centro_costo = models.ForeignKey(CentroCosto, models.SET_NULL, blank=True,null=True) 
-    sede = models.ForeignKey(Sede, models.SET_NULL, blank=True,null=True, default=1) 
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    id = models.AutoField(primary_key=True)
+    area_programa = models.ForeignKey(
+        AreaPrograma, models.SET_NULL, blank=True, null=True
+    )
+    regional = models.ForeignKey(Regional, models.SET_NULL, blank=True, null=True)
+    unidad_funcional = models.ForeignKey(
+        UnidadFuncional, models.SET_NULL, blank=True, null=True
+    )
+    punto_atencion = models.ForeignKey(
+        PuntoAtencion, models.SET_NULL, blank=True, null=True
+    )
+    centro_costo = models.ForeignKey(
+        CentroCosto, models.SET_NULL, blank=True, null=True
+    )
+    sede = models.ForeignKey(Sede, models.SET_NULL, blank=True, null=True, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'ParametrosAP - {self.id} - {self.regional} - {self.unidad_funcional} - {self.punto_atencion} - {self.centro_costo}' 
-    
+        return f"ParametrosAP - {self.id} - {self.regional} - {self.unidad_funcional} - {self.punto_atencion} - {self.centro_costo}"
+
+
 class Carga(models.Model):
     estados_carga = (
-        ('eliminada',"Carga eliminada"),
-        ('creada', "Carga creada"),
-        ('procesando', "Carga en proceso"),
-        ('procesada', "Carga procesada"), 
-        ('admisionando', "Carga en proceso de creación de admisiones"), 
-        ('cancelada', "Carga presentó errores durante su procesamiento"), 
+        ("eliminada", "Carga eliminada"),
+        ("creada", "Carga creada"),
+        ("procesando", "Carga en proceso"),
+        ("procesada", "Carga procesada"),
+        ("admisionando", "Carga en proceso de creación de admisiones"),
+        ("cancelada", "Carga presentó errores durante su procesamiento"),
     )
-    id = models.AutoField(primary_key =True)
-    usuario = models.ForeignKey(User, models.SET_NULL, blank=True,null=True)
-    data = models.JSONField(blank=True,null=True)
-    estado = models.CharField(max_length=20, default='creada', choices=estados_carga)
-    cantidad_actividades = models.IntegerField(default = 0)
-    cantidad_actividades_inconsistencias = models.IntegerField(default = 0)
-    cantidad_actividades_ok = models.IntegerField(default = 0)
-    cantidad_actividades_admisionadas = models.IntegerField(default = 0)
-    tiempo_procesamiento = models.FloatField(default = 0)
-    observacion = models.CharField(max_length= 500, null=True, blank=True)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    id = models.AutoField(primary_key=True)
+    usuario = models.ForeignKey(User, models.SET_NULL, blank=True, null=True)
+    data = models.JSONField(blank=True, null=True)
+    estado = models.CharField(max_length=20, default="creada", choices=estados_carga)
+    cantidad_actividades = models.IntegerField(default=0)
+    cantidad_actividades_inconsistencias = models.IntegerField(default=0)
+    cantidad_actividades_ok = models.IntegerField(default=0)
+    cantidad_actividades_admisionadas = models.IntegerField(default=0)
+    tiempo_procesamiento = models.FloatField(default=0)
+    observacion = models.CharField(max_length=500, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
-        return "Carga # {} ".format(self.id)
-    
+        return "Carga #{}-{}-{} ".format(self.id, self.estado, self.observacion)
+
     def actualizar_info_actividades(self):
-        actividades_carga = Actividad.objects.filter(carga = self)
+        actividades_carga = Actividad.objects.filter(carga=self)
         self.cantidad_actividades = actividades_carga.count()
-        self.cantidad_actividades_ok = actividades_carga.filter(inconsistencias = None, admision = None).count()
-        self.cantidad_actividades_admisionadas = actividades_carga.exclude(admision = None).filter(inconsistencias = None).count()
-        self.cantidad_actividades_inconsistencias = self.cantidad_actividades - self.cantidad_actividades_ok - self.cantidad_actividades_admisionadas
-    
+        self.cantidad_actividades_ok = actividades_carga.filter(
+            inconsistencias=None, admision=None
+        ).count()
+        self.cantidad_actividades_admisionadas = (
+            actividades_carga.exclude(admision=None)
+            .filter(inconsistencias=None)
+            .count()
+        )
+        self.cantidad_actividades_inconsistencias = (
+            self.cantidad_actividades
+            - self.cantidad_actividades_ok
+            - self.cantidad_actividades_admisionadas
+        )
+
+
 class TipoActividad(models.Model):
-    id = models.AutoField(primary_key =True)
-    grupo = models.CharField(max_length= 10, blank=True,null=True)
-    nombre = models.CharField(max_length= 150)
-    cups = models.CharField(max_length= 10)
-    responsable = models.CharField(max_length= 150, blank=True,null=True)
-    diagnostico = models.CharField(max_length= 10, blank=True,null=True)
-    finalidad = models.CharField(max_length= 50, blank=True,null=True)
-    fuente = models.CharField(max_length= 50 ,blank=True,null=True)
-    observacion = models.CharField(max_length= 150, blank=True,null=True)
-    entrega = models.CharField(max_length= 150, blank=True,null=True)
-    contrato = models.ForeignKey(ContratoMarco, models.SET_NULL, blank=True,null=True)
-    tipo_servicio = models.ForeignKey(TipoServicio, models.SET_NULL, blank=True,null=True) #fuetnes tips # VALIDAR
-    area = models.ForeignKey(AreaPrograma, models.SET_NULL, blank=True,null=True) 
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    id = models.AutoField(primary_key=True)
+    grupo = models.CharField(max_length=10, blank=True, null=True)
+    nombre = models.CharField(max_length=150)
+    cups = models.CharField(max_length=10)
+    responsable = models.CharField(max_length=150, blank=True, null=True)
+    diagnostico = models.CharField(max_length=10, blank=True, null=True)
+    finalidad = models.CharField(max_length=50, blank=True, null=True)
+    fuente = models.CharField(max_length=50, blank=True, null=True)
+    observacion = models.CharField(max_length=150, blank=True, null=True)
+    entrega = models.CharField(max_length=150, blank=True, null=True)
+    contrato = models.ForeignKey(ContratoMarco, models.SET_NULL, blank=True, null=True)
+    tipo_servicio = models.ForeignKey(
+        TipoServicio, models.SET_NULL, blank=True, null=True
+    )  # fuetnes tips # VALIDAR
+    area = models.ForeignKey(AreaPrograma, models.SET_NULL, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'TipoActividad - {self.id} - {self.nombre} - {self.cups} - {self.diagnostico}'
-    
+        return f"TipoActividad - {self.id} - {self.nombre} - {self.cups} - {self.diagnostico}"
+
+
 class Actividad(models.Model):
-    
-    id = models.AutoField(primary_key =True)
-    tipo_fuente = models.CharField(max_length= 10)
-    admision = models.ForeignKey(Admision, models.SET_NULL, blank=True,null=True)
-    identificador = models.CharField(max_length= 150) 
-    regional = models.CharField(max_length= 150)
+
+    id = models.AutoField(primary_key=True)
+    tipo_fuente = models.CharField(max_length=10)
+    admision = models.ForeignKey(Admision, models.SET_NULL, blank=True, null=True)
+    identificador = models.CharField(max_length=150)
+    regional = models.CharField(max_length=150)
     fecha_servicio = models.DateField()
-    nombre_actividad = models.CharField(max_length= 250)
-    tipo_actividad = models.ForeignKey(TipoActividad, models.SET_NULL, blank=True,null=True) 
-    diagnostico_p = models.CharField(max_length= 10, default= "")
-    diagnostico_1 = models.CharField(max_length= 10, default= "")
-    diagnostico_2 = models.CharField(max_length= 10, default= "")
-    diagnostico_3 = models.CharField(max_length= 10, default= "")
-    tipo_documento = models.CharField(max_length= 10, default="CC")
-    documento_paciente = models.CharField(max_length= 50)
-    nombre_paciente = models.CharField(max_length= 200)
-    parametros_programa =  models.ForeignKey(ParametrosAreaPrograma, models.SET_NULL, blank=True,null=True) 
-    carga =  models.ForeignKey(Carga, models.SET_NULL, blank=True,null=True) 
-    inconsistencias = models.CharField(max_length= 500, null=True, blank=True)
-    documento_medico = models.CharField(max_length= 50, blank=True,null=True)
-    medico = models.ForeignKey(Medico, models.SET_NULL, blank=True,null=True)
+    nombre_actividad = models.CharField(max_length=250)
+    tipo_actividad = models.ForeignKey(
+        TipoActividad, models.SET_NULL, blank=True, null=True
+    )
+    diagnostico_p = models.CharField(max_length=10, default="")
+    diagnostico_1 = models.CharField(max_length=10, default="")
+    diagnostico_2 = models.CharField(max_length=10, default="")
+    diagnostico_3 = models.CharField(max_length=10, default="")
+    tipo_documento = models.CharField(max_length=10, default="CC")
+    documento_paciente = models.CharField(max_length=50)
+    nombre_paciente = models.CharField(max_length=200)
+    parametros_programa = models.ForeignKey(
+        ParametrosAreaPrograma, models.SET_NULL, blank=True, null=True
+    )
+    carga = models.ForeignKey(Carga, models.SET_NULL, blank=True, null=True)
+    inconsistencias = models.CharField(max_length=500, null=True, blank=True)
+    documento_medico = models.CharField(max_length=50, blank=True, null=True)
+    medico = models.ForeignKey(Medico, models.SET_NULL, blank=True, null=True)
     admisionada_otra_carga = models.BooleanField(default=False)
-    datos_json = models.JSONField(blank=True,null=True) 
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    datos_json = models.JSONField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'Actividad - {self.id} - {self.fecha_servicio} - {self.nombre_actividad}' 
+        return (
+            f"Actividad - {self.id} - {self.fecha_servicio} - {self.nombre_actividad}"
+        )
+
 
 class TokenApiZeus(models.Model):
     vigencia = 1
 
-    id = models.AutoField(primary_key =True)
+    id = models.AutoField(primary_key=True)
     token = models.CharField(max_length=250)
     vigente = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add = True)
-    updated_at = models.DateTimeField(auto_now = True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return "Token # {} ".format(self.id)
-    
+
     def validar_vencimiento(self):
         print("Validar vencimiento: ", datetime.datetime.today(), self.updated_at)
         dias_uso = (datetime.date.today() - self.updated_at.date()).days
         print("DÍAS TOKEN:", dias_uso)
-        if dias_uso > self.vigencia: # Validar si No está vigente
-            self.vigente = False 
+        if dias_uso > self.vigencia:  # Validar si No está vigente
+            self.vigente = False
             self.save()
-
-        
-
-
-
-
